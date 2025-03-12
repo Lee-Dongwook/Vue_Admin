efaa1976b521e795911ac5c010624bc7d9242f8a
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-03-12 11:48:58 +0900

    feat : error

---
+++ src/views/error/403.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import noAccess from "@/assets/status/403.svg?component";
+
+defineOptions({
+  name: "403"
+});
+
+const router = useRouter();
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-[640px]">
+    <noAccess />
+    <div class="ml-12">
+      <p
+        v-motion
+        class="font-medium text-4xl mb-4 dark:text-white"
+        :initial="{
+          opacity: 0,
+          y: 100
+        }"
+        :enter="{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 80
+          }
+        }"
+      >
+        403
+      </p>
+      <p
+        v-motion
+        class="mb-4 text-gray-500"
+        :initial="{
+          opacity: 0,
+          y: 100
+        }"
+        :enter="{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 120
+          }
+        }"
+      >
+        죄송합니다. 이 페이지에 엑세스할 수 있는 권한이 없습니다.
+      </p>
+      <el-button
+        v-motion
+        type="primary"
+        :initial="{
+          opacity: 0,
+          y: 100
+        }"
+        :enter="{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 160
+          }
+        }"
+        @click="router.push('/')"
+      >
+        홈페이지로 돌아가기
+      </el-button>
+    </div>
+  </div>
+</template>
---
+++ src/views/error/404.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import noExist from "@/assets/status/404.svg?component";
+
+defineOptions({
+  name: "404"
+});
+
+const router = useRouter();
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-[640px]">
+    <noExist />
+    <div class="ml-12">
+      <p
+        v-motion
+        class="font-medium text-4xl mb-4 dark:text-white"
+        :initial="{
+          opacity: 0,
+          y: 100
+        }"
+        :enter="{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 80
+          }
+        }"
+      >
+        404
+      </p>
+      <p
+        v-motion
+        class="mb-4 text-gray-500"
+        :initial="{
+          opacity: 0,
+          y: 100
+        }"
+        :enter="{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 120
+          }
+        }"
+      >
+        죄송합니다. 방문한 페이지가 존재하지 않습니다.
+      </p>
+      <el-button
+        v-motion
+        type="primary"
+        :initial="{
+          opacity: 0,
+          y: 100
+        }"
+        :enter="{
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: 160
+          }
+        }"
+        @click="router.push('/')"
+      >
+        홈페이지로 돌아가기
+      </el-button>
+    </div>
+  </div>
+</template>
